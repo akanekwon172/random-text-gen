d2155395fd896c51375491b16222db32222acb52
akanekwon172 <akanekwon_dev@icloud.com>
2023-07-27 18:57:19 +0900

    漢字ブロックの追加

--- src/js/generate.js
+++ src/js/generate.js
@@ -1,30 +1,43 @@
 'use strict';
 
-import { DEFAULT_BLOCKS, UNICODE_BLOCKS, randomUnicodeChar} from './unicode-blocks.js';
+import { BASIC_BLOCKS, UNICODE_BLOCKS, randomUnicodeChar } from './unicode-blocks.js';
 
 const basicBlockList = document.querySelectorAll('input[name=blocks]');
+const unicodeBlockList = document.querySelectorAll('input[name=ucblocks]');
 const textLength = document.querySelector('#textLength');
 const createCount = document.querySelector('#createCount');
 const generateButton = document.querySelector('#generateButton');
+const resetButton = document.querySelector('#resetButton');
+const resultList = document.querySelector('#result');
 
 const MAX_LENGTH = 20;
 const MAX_COUNT = 10;
 let checkedArray = [];
+let checkedKanjiArray = [];
 
 /**
  * 選択された文字種からランダムに 1つ選択
  * @param array
- * @returns
+ * @returns {number}
  */
 const randomBlock = (array) => Math.floor(Math.random() * array.length);
 
-
+/**
+ * 半角文字と漢字ブロックを切り替える
+ */
+const toggleBlockList = () => {
+  if ([...unicodeBlockList].some((c) => c.checked)) {
+    [...basicBlockList].every((c) => (c.disabled = true));
+  } else {
+    [...basicBlockList].forEach((c) => (c.disabled = false));
+  }
+};
 
 /**
  * 選択された文字種のみを checkedArray に更新
  * @param array
  * @param checkbox
- * @returns array
+ * @returns {array}
  */
 const toggleCheckedArray = (array, checkbox) => {
   if (array.includes(checkbox.value)) {
@@ -41,6 +54,12 @@ basicBlockList.forEach((checkbox) => {
   });
 });
 
+unicodeBlockList.forEach((checkbox) => {
+  checkbox.addEventListener('change', () => {
+    toggleBlockList();
+    checkedKanjiArray = toggleCheckedArray(checkedKanjiArray, checkbox);
+  });
+});
 
 generateButton.addEventListener('click', (e) => {
   e.preventDefault();
@@ -58,8 +77,13 @@ generateButton.addEventListener('click', (e) => {
     let result = [];
 
     for (let i = 0; i < length; i++) {
-      // unicode-blocks.js 内で定義
-      const block = DEFAULT_BLOCKS[checkedArray[randomBlock(checkedArray)]];
+      const block = BASIC_BLOCKS[checkedArray[randomBlock(checkedArray)]];
+      const block2 = UNICODE_BLOCKS[checkedKanjiArray[randomBlock(checkedKanjiArray)]];
+
+      if ([...unicodeBlockList].some((c) => c.checked)) {
+        result = [...result, randomUnicodeChar(block2.range.from, block2.range.to)];
+        continue;
+      }
 
       if (block.name === '記号') {
         let randomKigou = [];
@@ -67,7 +91,7 @@ generateButton.addEventListener('click', (e) => {
         block.ranges.forEach((range) => {
           randomKigou = [...randomKigou, randomUnicodeChar(range.from, range.to)];
         });
-        // 複数の range からランダムに1つ代入
+        // 複数の range からランダムに 1つ代入
         result = [...result, randomKigou[Math.floor(Math.random() * block.ranges.length)]];
       } else {
         result = [...result, randomUnicodeChar(block.range.from, block.range.to)];
@@ -89,4 +113,10 @@ window.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('input[name=blocks]:checked').forEach((checkbox) => {
     checkedArray = [...checkedArray, checkbox.value];
   });
+
+  document.querySelectorAll('input[name=ucblocks]:checked').forEach((checkbox) => {
+    checkedKanjiArray = [...checkedKanjiArray, checkbox.value];
+  });
+
+  toggleBlockList();
 });
--- src/js/unicode-blocks.js
+++ src/js/unicode-blocks.js
@@ -1,10 +1,10 @@
 'use strict';
 
-// ユニコードブロックの定義
-const DEFAULT_BLOCKS = [
-  { name: '数字', range: { from: 0x30, to: 0x39 } },
-  { name: '英字(大文字)', range: { from: 0x41, to: 0x5a } },
-  { name: '英字(小文字)', range: { from: 0x61, to: 0x7a } },
+/** ユニコードブロック(半角文字) の定義 */
+const BASIC_BLOCKS = [
+  { name: '数字',      range: { from: 0x30, to: 0x39 }},
+  { name: '英字(大文字)', range: { from: 0x41, to: 0x5a }},
+  { name: '英字(小文字)', range: { from: 0x61, to: 0x7a }},
   {
     name: '記号',
     ranges: [
@@ -16,16 +16,30 @@ const DEFAULT_BLOCKS = [
   },
 ];
 
+/** ユニコードブロック(漢字) の定義 */
+const UNICODE_BLOCKS = [
+  { name: 'CJK統合漢字拡張A', range: { from:  0x3400, to:  0x4dbf }},
+  { name: 'CJK統合漢字',    range: { from:  0x4e00, to:  0x9fff }},
+  { name: 'CJK互換漢字',    range: { from:  0xf900, to:  0xfaff }},
+
+/*   { name: 'CJK統合漢字拡張B', range: { from: 0x20000, to: 0x2a6df }},
+  { name: 'CJK統合漢字拡張C', range: { from: 0x2a700, to: 0x2b73f }},
+  { name: 'CJK統合漢字拡張D', range: { from: 177984, to: 178207 }},
+  { name: 'CJK統合漢字拡張E', range: { from: 178208, to: 183983 }},
+  { name: 'CJK統合漢字拡張F', range: { from: 183984, to: 191471 }},
+  { name: 'CJK互換漢字補助',  range: { from: 194560, to: 195103 }}, */
+];
+
 /**
  * ランダムな文字を生成
  * @param min
  * @param max
- * @returns string
+ * @returns {string}
  */
 const randomUnicodeChar = (min = 0x21, max = 0x7e) => {
   if (min > max) [min, max] = [max, min];
 
-  return String.fromCharCode(Math.floor(Math.random() * (max - min + 1) + min));
+   return String.fromCodePoint(Math.floor(Math.random() * (max - min + 1) + min));
 };
 
-export { DEFAULT_BLOCKS, UNICODE_BLOCKS, randomUnicodeChar };
+export { BASIC_BLOCKS, UNICODE_BLOCKS, randomUnicodeChar };
