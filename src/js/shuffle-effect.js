db18e96a7aab4cab232e509016a73908ad5b0fd2
akanekwon172 <akanekwon_dev@icloud.com>
2023-09-11 03:14:01 +0900

    スタイルの調整

--- src/js/shuffle-effect.js
+++ src/js/shuffle-effect.js
@@ -2,12 +2,12 @@ import { randomUnicodeChar } from './unicode-blocks.js';
 
 /** 文字列のシャッフルアニメーション */
 class ShuffleEffect {
+  #idNum = 0;
+  #frame = 0;
   #rafId = null;
 
   constructor(id, element) {
     this.id = id;
-    this.idx = 0;
-    this.frame = 0;
     this.element = element;
     this.element.className = `${id}`;
     this.originalString = element.innerText;
@@ -26,29 +26,29 @@ class ShuffleEffect {
     this.spans = [...this.element.querySelectorAll('span')];
   }
 
-  /** Unicode [from ... to] の範囲で frame 毎に randomUnicodeChar を代入 */
+  /** Unicodeの [from ... to] の範囲で frame % number 毎に randomUnicodeChar を代入 */
   animate(from, to) {
-    if (this.idx !== this.originalString.length) {
-      this.spans[this.idx].style.opacity = 1;
-
-      if (this.frame % 6 === 0) {
-        this.spans[this.idx].innerHTML = randomUnicodeChar(from, to);
-      }
-      if (this.frame % 60 === 0 && this.frame !== 0) {
-        this.spans[this.idx].innerHTML = this.originalString[this.idx];
-        this.idx += 1;
-      }
-
-      this.frame += 1;
-
+    if (this.#idNum !== this.originalString.length) {
       this.#rafId = requestAnimationFrame(this.animate.bind(this, from, to));
+      this.spans[this.#idNum].style.opacity = 1;
+
+      if (this.#frame % 6 === 0) {
+        this.spans[this.#idNum].innerHTML = randomUnicodeChar(from, to);
+      }
+
+      if (this.#frame % 60 === 0 && this.#frame !== 0) {
+        this.spans[this.#idNum].innerHTML = this.originalString[this.#idNum];
+        this.#idNum += 1;
+      }
+
+      this.#frame += 1;
     }
   }
 
   /** 文字列を非表示状態にする */
   reset() {
-    this.idx = 0;
-    this.frame = 0;
+    this.#idNum = 0;
+    this.#frame = 0;
 
     [...this.spans].forEach((span) => {
       span.style.opacity = 0;
